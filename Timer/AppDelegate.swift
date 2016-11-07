0aaabe8c90d89678c994aa3e1062bce1da1c89e4
110 <110@MacBook.local>
2016-11-08 01:15:08 +0700

    show argument send from WorkoutSolution

--- Timer/AppDelegate.swift
+++ Timer/AppDelegate.swift
@@ -19,6 +19,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         return true
     }
 
+    func application(_ application: UIApplication, handleOpen url: URL) -> Bool {
+        let value = url.absoluteString
+        let urlArray = value.components(separatedBy: "//")
+        let message = urlArray[1]
+        let alert = UIAlertView()
+        alert.message = message
+        alert.show()
+        return true
+    }
+
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
