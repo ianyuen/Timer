2bc51f82457ed68a310730921f916a3ddb357192
Thanh Long <ian@ians-MacBook.local>
2017-01-15 16:01:43 +0700

    data base for history details

--- Timer/CustomObject/HistoryDetailsScroll.swift
+++ Timer/CustomObject/HistoryDetailsScroll.swift
@@ -75,40 +75,60 @@ class HistoryDetailsScroll: ScrollView {
 		}
 	}
 
-	func AddHistoryDetail(_ historyDetail: HistoryDetail, object: ScreenObject) {
+	func AddHistoryDetail(_ historyDetail: HistoryDetail, object: ScreenObject, content: String) {
 		historyDetail.SetTitle(object.text)
-		//historyDetail.SetContent(dateContent)
+		historyDetail.SetContent(content)
 		objectManager.AddView(historyDetail, parent: self, object: object)
 	}
 
 	func FindHistoryDetail(_ object: ScreenObject) {
+		let sessions = Database.instance.ReadSessions("sessions")
+		let session = sessions[Application.instance.SessionIndex()]
+
+		var content = ""
 		var historyDetail = HistoryDetail()
 		switch object.name {
 		case "date":
+			let nsDate = NSDate(timeIntervalSince1970: session.epoch)
+			let dateFormatter = DateFormatter()
+			dateFormatter.dateFormat = "MMM/dd/YYYY"
+			content = dateFormatter.string(from: nsDate as Date)
 			historyDetail = date
 		case "time":
+			let nsDate = NSDate(timeIntervalSince1970: session.epoch)
+			let dateFormatter = DateFormatter()
+			dateFormatter.dateFormat = "hh:mm a"
+			content = dateFormatter.string(from: nsDate as Date)
 			historyDetail = time
 		case "warmUpTime":
+			content = String(session.warmUpTime) + "'"
 			historyDetail = warmUpTime
 		case "restTime":
+			content = String(session.restTime) + "'"
 			historyDetail = restTime
 		case "coolDownTime":
+			content = String(session.coolDownTime) + "'"
 			historyDetail = coolDownTime
 		case "roundTime":
+			content = String(session.roundTime) + "'"
 			historyDetail = roundTime
 		case "totalRounds":
+			content = String(session.totalRounds)
 			historyDetail = totalRounds
 		case "rounds":
+			content = String(session.rounds) + "/" + String(session.totalRounds)
 			historyDetail = rounds
 		case "training":
+			content = session.training
 			historyDetail = training
-		case "totalRounds":
-			historyDetail = totalRounds
 		case "totalTrainingTime":
+			let minute = session.totalTrainingTime / 60
+			let second = session.totalTrainingTime % 60
+			content = String(minute) + "'" + String(second) + "'"
 			historyDetail = totalTrainingTime
 		default: break
 		}
 
-		AddHistoryDetail(historyDetail, object: object)
+		AddHistoryDetail(historyDetail, object: object, content: content)
 	}
 }
--- Timer/Database/Session.swift
+++ Timer/Database/Session.swift
@@ -10,20 +10,50 @@ import Foundation
 
 class Session: NSObject, NSCoding {
 	var epoch: Double
+	var rounds: Int
+	var restTime: Int
 	var training: String
+	var roundTime: Int
+	var warmUpTime: Int
+	var totalRounds: Int
+	var coolDownTime: Int
+	var totalTrainingTime: Int
 
 	func encode(with coder: NSCoder) {
 		coder.encode(epoch, forKey: "epoch")
+		coder.encode(rounds, forKey: "rounds")
+		coder.encode(restTime, forKey: "restTime")
 		coder.encode(training, forKey: "training")
+		coder.encode(roundTime, forKey: "roundTime")
+		coder.encode(warmUpTime, forKey: "warmUpTime")
+		coder.encode(totalRounds, forKey: "totalRounds")
+		coder.encode(coolDownTime, forKey: "coolDownTime")
+		coder.encode(totalTrainingTime, forKey: "totalTrainingTime")
 	}
 
 	required init(coder decoder: NSCoder) {
 		epoch = decoder.decodeDouble(forKey: "epoch")
+		rounds = decoder.decodeInteger(forKey: "rounds")
+		restTime = decoder.decodeInteger(forKey: "restTime")
 		training = decoder.decodeObject(forKey: "training") as! String
+		roundTime = decoder.decodeInteger(forKey: "roundTime")
+		warmUpTime = decoder.decodeInteger(forKey: "warmUpTime")
+		totalRounds = decoder.decodeInteger(forKey: "totalRounds")
+		coolDownTime = decoder.decodeInteger(forKey: "coolDownTime")
+		totalTrainingTime = decoder.decodeInteger(forKey: "totalTrainingTime")
+
 	}
 
-	required init(training: String = "Workout", epoch: Double = 1484449200) {
+	required init(training: String = "Workout", epoch: Double = 1484449200, totalRounds: Int = 7, rounds: Int = 7, roundTime: Int = 20, restTime: Int = 20, warmUpTime: Int = 20, coolDownTime: Int = 20, totalTrainingTime: Int = 70) {
 		self.epoch = epoch
 		self.training = training
+
+		self.rounds = rounds
+		self.restTime = restTime
+		self.roundTime = roundTime
+		self.warmUpTime = warmUpTime
+		self.totalRounds = totalRounds
+		self.coolDownTime = coolDownTime
+		self.totalTrainingTime = totalTrainingTime
 	}
 }
